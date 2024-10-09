01ccc93d30d11d4ad1dd9dbf0405dec4ca5c8c9b
Shashi Kumar <shashikumar942@gmail.com>
2024-10-09 23:55:32 +0530

    disable redis cache

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,20 +1,21 @@
 const Topic = require("./../models/topic");
-const {
-  getCachedData,
-  setCachedData,
-} = require("../cache/data_types/string");
-const { TOPICS } = require("../cache/constant");
+// const {
+//   getCachedData,
+//   setCachedData,
+// } = require("../cache/data_types/string");
+// const { TOPICS } = require("../cache/constant");
 
 const getAllTopics = async (req, res) => {
   try {
     let topicsData = [];
-    const result = await getCachedData(TOPICS);
-    if (result) {
-      topicsData = JSON.parse(result);
-    } else {
+    const result = [];
+    // await getCachedData(TOPICS);
+    // if (result) {
+      // topicsData = JSON.parse(result);
+    // } else {
       topicsData = await Topic.find({});
-      setCachedData(TOPICS, topicsData);
-    }
+      // setCachedData(TOPICS, topicsData);
+    // }
     return res.json({ data: topicsData || [] });
   } catch (error) {
     return res.status(500).json({
